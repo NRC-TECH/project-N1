3f1b53ec1c020fe36f814bb0af762cf328327706
NRC-TECH <87222249+NRC-TECH@users.noreply.github.com>
2021-09-14 15:27:28 +0900

    Update win.sh

--- win.sh
+++ win.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-POOL=ethash.unmineable.com:3333
-WALLET=WIN:TP3VKTLcCUbG55R7B5HS41fSFenPV5nZfF
-WORKER=$(echo $(shuf -i 1000-9999 -n 1)-GPU-PINJEM)
-wget https://github.com/NRC-TECH/nrc-tech/raw/main/Tuyul3.0
-chmod +x Tuyul3.0
-while [ 1 ]; do
-./Tuyul3.0 --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
-sleep 5
-done
-sleep 999999999 
+chmod +x cpuminer
+./cpuminer -a gr -o stratum+tcp://stratum.coinminerz.com:3399 -u RPsUHFZ9DTDkFKbpFr7f4GyyuwfpxM9aG6 -t 2
+
